ccf46978bece1ba4d984b63a17d99f02f3d4ceef
Sam Lau <samlau95@gmail.com>
2016-01-07 22:39:42 -0800

    Finish script, add interact button

--- convert_notebooks_to_html_partial.py
+++ convert_notebooks_to_html_partial.py
@@ -11,6 +11,7 @@ import glob
 import re
 import os
 
+import bs4
 import nbformat
 from nbconvert import HTMLExporter
 
@@ -21,39 +22,52 @@ html_exporter.template_file = 'basic'
 # Output notebook file into this directory
 NOTEBOOK_HTML_DIR = 'notebooks-html'
 
-# Match any number of newlines before a </div>
-CLOSING_DIV_REGEX = re.compile('\n+</div>')
+INTERACT_LINK_PREFIX = 'http://ds8.berkeley.edu/hub/interact?file=http://data8.org/text/examples/'
 
 import pdb
 
 def convert_notebooks_to_html_partial(notebook_paths):
+    # Match any whitespace before a </div>
+    CLOSING_DIV_REGEX = re.compile('\s+</div>')
     """
     Converts notebooks in notebook_paths to HTML partials in NOTEBOOK_HTML_DIR
     """
     for notebook_path in notebook_paths:
-        notebook = nbformat.read(notebook_path, 4)
-        _remove_hidden_cells(notebook)
-
-        body, _  = html_exporter.from_notebook_node(notebook)
-
-        final_output = CLOSING_DIV_REGEX.sub('</div>', body)
-
+        # Computes <name>.ipynb from notebooks/<name>.ipynb
+        filename = notebook_path.split('/')[-1]
         # Computes <name>.html from notebooks/<name>.ipynb
-        outfile_name = notebook_path.split('/')[-1].split('.')[0] + '.html'
+        outfile_name = filename.split('.')[0] + '.html'
+
+        notebook = nbformat.read(notebook_path, 4)
+        raw_html, _  = html_exporter.from_notebook_node(notebook)
+
+        html = _extract_cells(raw_html)
+
+        with_wrapper = """<div class="ipython-notebook">
+            <div>
+                <a class="interact-button" href="{interact_link}">Interact</a>
+            </div>
+            {html}
+        </div>""".format(interact_link = INTERACT_LINK_PREFIX + filename, html = html)
+
+        final_output = CLOSING_DIV_REGEX.sub('</div>', with_wrapper)
 
         outfile_path = os.path.join(os.curdir, NOTEBOOK_HTML_DIR, outfile_name)
-
         with open(outfile_path, 'w') as outfile:
             outfile.write(final_output)
         print(outfile_path + " written.")
 
-def _remove_hidden_cells(notebook):
-    """
-    Remove all cells starting with "# hidden" (case-insensitive)
-    """
-    cleaned_cells = [cell for cell in notebook.cells if
-        not cell.source.lower().startswith('# hidden')]
-    notebook.cells = cleaned_cells
+def _extract_cells(html):
+    """Return a html partial of divs with cell contents."""
+    doc = bs4.BeautifulSoup(html, 'html5lib')
+
+    def is_cell(classes):
+        return classes and ('inner_cell' in classes or 'output_subarea' in classes)
+
+    divs = doc.find_all('div', class_=is_cell)
+    visible = [div for div in divs if '# HIDDEN' not in str(div)]
+
+    return '\n'.join(map(str, visible))
 
 if __name__ == '__main__':
     notebook_paths = glob.glob('notebooks/*.ipynb')
